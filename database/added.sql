967c2727f14098e9247f366685b9c8d1caa87acf
Matteo Di Noia <dmatteo002@gmail.com>
2024-06-01 18:05:11 +0200

    adding setup of sql database

--- database/added.sql
+++ database/added.sql
@@ -1,1 +1,29 @@
- 
+USE catering;
+
+DROP TABLE IF EXISTS `Tasks`;
+
+CREATE TABLE `Tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `recipe_id` INT(11) NOT NULL,
+  `expiration` date DEFAULT NULL,
+  `quantity` INT(11) DEFAULT NULL,
+  `duration_min` INT(11) DEFAULT NULL,
+  `completed` BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `TaskService`;
+
+CREATE TABLE `TaskService` (
+  `task_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `TaskTurn`;
+
+CREATE TABLE `TaskTurn` (
+  `task_id` int(11) NOT NULL,
+  `turn_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- java/catering/src/main/java/catering/TestTask1.java
+++ java/catering/src/main/java/catering/TestTask1.java
@@ -3,11 +3,7 @@ package catering;
 import catering.businesslogic.CatERing;
 import catering.businesslogic.UseCaseLogicException;
 import catering.businesslogic.event.ServiceInfo;
-import catering.businesslogic.menu.Menu;
-import catering.businesslogic.menu.MenuItem;
-import catering.businesslogic.menu.Section;
 import catering.businesslogic.recipe.Recipe;
-import javafx.collections.ObservableList;
 
 public class TestTask1 {
     public static void main(String[] args) {
